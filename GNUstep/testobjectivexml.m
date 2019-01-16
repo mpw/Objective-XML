551bbe083363401685e87144dfcfd5bfa6932503
Marcel Weiher <marcel@metaobject.com>
2019-01-16 15:52:34 +0100

    replaced XmlParser in tests with MAXParser, its superclass: just 1 failure for 28 successes

--- GNUstep/testobjectivexml.m
+++ GNUstep/testobjectivexml.m
@@ -27,9 +27,9 @@ static void runTests()
 	int success=0;
 	int failure=0;
 	NSArray *classes=@[
-//        @"MPWMAXParser",
+        @"MPWMAXParser",
         @"MPWTagHandlerTesting",
-        @"MPWXmlParserTesting",
+//        @"MPWXmlParserTesting",
 	];
 
 	for (NSString *className in classes ) {
